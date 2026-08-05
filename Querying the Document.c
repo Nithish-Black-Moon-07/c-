f9a3d58fd06848a8216ed80fb545aedc65b12915
Nithish-Black-Moon-07 <bala465465_bci28@mepcoeng.ac.in>
2026-08-05 15:50:56 +0530

    Add document querying functionality in C

---
+++ Querying the Document.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include<assert.h>
+#define MAX_CHARACTERS 1005
+#define MAX_PARAGRAPHS 5
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* kth_word_in_mth_sentence_of_nth_paragraph(char**** document, int k, int m, int n) {
+    // 1-based index conversion to 0-based index
+    return document[n - 1][m - 1][k - 1];
+}
+
+char** kth_sentence_in_mth_paragraph(char**** document, int k, int m) {
+    // 1-based index conversion to 0-based index
+    return document[m - 1][k - 1];
+}
+
+char*** kth_paragraph(char**** document, int k) {
+    // 1-based index conversion to 0-based index
+    return document[k - 1];
+}
+
+char**** get_document(char* text) {
+    char ****document = malloc(sizeof(char ***));
+    document[0] = malloc(sizeof(char **));
+    document[0][0] = malloc(sizeof(char *));
+    
+    int p = 0; // Paragraph index
+    int s = 0; // Sentence index
+    int w = 0; // Word index
+    
+    char *word = malloc(100 * sizeof(char));
+    int char_count = 0;
+    
+    while (*text) {
+        if (*text == ' ') {
+            word[char_count] = '\0';
+            document[p][s][w] = malloc((char_count + 1) * sizeof(char));
+            strcpy(document[p][s][w], word);
+            w++;
+            
+            document[p][s] = realloc(document[p][s], (w + 1) * sizeof(char *));
+            char_count = 0;
+        } else if (*text == '.') {
+            word[char_count] = '\0';
+            document[p][s][w] = malloc((char_count + 1) * sizeof(char));
+            strcpy(document[p][s][w], word);
+            w = 0;
+            s++;
+            
+            document[p] = realloc(document[p], (s + 1) * sizeof(char **));
+            document[p][s] = malloc(sizeof(char *));
+            char_count = 0;
+        } else if (*text == '\n') {
+            w = 0;
+            s = 0;
+            p++;
+            
+            document = realloc(document, (p + 1) * sizeof(char ***));
+            document[p] = malloc(sizeof(char **));
+            document[p][0] = malloc(sizeof(char *));
+            char_count = 0;
+        } else {
+            word[char_count++] = *text;
+        }
+        text++;
+    }
+    free(word);
+    return document;
+}
+
+
+
+
+
+char* get_input_text() {	
+    int paragraph_count;
+    scanf("%d", &paragraph_count);
+
+    char p[MAX_PARAGRAPHS][MAX_CHARACTERS], doc[MAX_CHARACTERS];
+    memset(doc, 0, sizeof(doc));
+    getchar();
+    for (int i = 0; i < paragraph_count; i++) {
+        scanf("%[^\n]%*c", p[i]);
+        strcat(doc, p[i]);
+        if (i != paragraph_count - 1)
+            strcat(doc, "\n");
+    }
+
+    char* returnDoc = (char*)malloc((strlen (doc)+1) * (sizeof(char)));
+    strcpy(returnDoc, doc);
+    return returnDoc;
+}
+
+void print_word(char* word) {
+    printf("%s", word);
+}
+
+void print_sentence(char** sentence) {
+    int word_count;
+    scanf("%d", &word_count);
+    for(int i = 0; i < word_count; i++){
+        printf("%s", sentence[i]);
+        if( i != word_count - 1)
+            printf(" ");
+    }
+} 
+
+void print_paragraph(char*** paragraph) {
+    int sentence_count;
+    scanf("%d", &sentence_count);
+    for (int i = 0; i < sentence_count; i++) {
+        print_sentence(*(paragraph + i));
+        printf(".");
+    }
+}
+
+int main() 
+{
+    char* text = get_input_text();
+    char**** document = get_document(text);
+
+    int q;
+    scanf("%d", &q);
+
+    while (q--) {
+        int type;
+        scanf("%d", &type);
+
+        if (type == 3){
+            int k, m, n;
+            scanf("%d %d %d", &k, &m, &n);
+            char* word = kth_word_in_mth_sentence_of_nth_paragraph(document, k, m, n);
+            print_word(word);
+        }
+
+        else if (type == 2){
+            int k, m;
+            scanf("%d %d", &k, &m);
+            char** sentence = kth_sentence_in_mth_paragraph(document, k, m);
+            print_sentence(sentence);
+        }
+
+        else{
+            int k;
+            scanf("%d", &k);
+            char*** paragraph = kth_paragraph(document, k);
+            print_paragraph(paragraph);
+        }
+        printf("\n");
+    }     
+}
